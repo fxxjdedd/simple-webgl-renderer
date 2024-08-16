fd2a223e98e8c06c45d22ffc6487105be3c692a0
fxxjdedd <fxxjdedd@qq.com>
2024-08-16 23:49:18 +0800

    support obj loader

--- src/gl/glIndexBuffer.ts
+++ src/gl/glIndexBuffer.ts
@@ -3,12 +3,17 @@ import { BufferLayout, IndexStructuredData, StructuredData } from "../util";
 export class GL_IndexBuffer {
     glBuffer: WebGLBuffer;
     structuredData: IndexStructuredData;
-    constructor(public gl: WebGL2RenderingContext, indexData: number[]) {
+    constructor(public gl: WebGL2RenderingContext) {
         this.glBuffer = gl.createBuffer();
         this.structuredData = new IndexStructuredData();
+    }
+
+    updateBufferData(indexData: number[]) {
+        const gl = this.gl;
         this.structuredData.merge({
             index: indexData,
         });
+
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.glBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.structuredData.buffer, gl.STATIC_DRAW);
     }
